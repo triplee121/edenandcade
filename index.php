225f237cc932e23433c63cbcf67532f7932f30ab
triplee121 <eden.englert@gmail.com>
2014-12-20 22:05:05 -0600

    Favicon

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>Eden and Cade</title>
+<!-- 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
+<!-- 	<link rel="icon" href="/favicon.ico" type="image/x-icon"> -->
 </head>
 <body>
 <?php echo "<h1>This is Eden's first major website project. Hello, how are you doing today?</h1>";	?>
