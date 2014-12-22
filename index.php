5638dc4c1c41de05e9430b68bfb6a3651dc93557
triplee121 <eden.englert@gmail.com>
2014-12-21 22:24:28 -0600

    Added favicon and background image. For some reason only the rainbow background image works. Need to find out if only certain dimensions work.

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>Eden and Cade</title>
-<!-- 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
-<!-- 	<link rel="icon" href="/favicon.ico" type="image/x-icon"> -->
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<link rel="stylesheet" type="text/css" href="CSS/index.css">
 </head>
 <body>
-<?php echo "<h1>This is Eden's first major website project. Hello, how are you doing today?</h1>";	?>
+<?php echo "<h1>Eden</h1>"?>
+<!-- <?php echo "<div style ='font:11px/21px Arial,tahoma,sans-serif;color:blue'> Movie List 2013</div>"?> -->
 </body>
 </html>
